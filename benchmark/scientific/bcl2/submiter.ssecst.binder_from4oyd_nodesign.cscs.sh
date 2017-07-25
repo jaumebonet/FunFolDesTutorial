5d0283800f4b50b61fd247af13d088547ed3e7ad
Jaume Bonet <jaume.bonet@gmail.com>
2017-07-25 21:32:20 +0200

    bcl2 no-design fullcst test

---
+++ benchmark/scientific/bcl2/submiter.ssecst.binder_from4oyd_nodesign.cscs.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -l
+#SBATCH --ntasks=96
+#SBATCH --ntasks-per-node=12
+#SBATCH --cpus-per-task 1
+#SBATCH --time 05:00:00
+#SBATCH --partition=normal
+#SBATCH --constraint=gpu
+#SBATCH --job-name="bcl2_ssecst_binder_nodesign"
+#SBATCH --output=bcl2_ssecst_binder_nodesign_%A.out
+#SBATCH --error=bcl2_ssecst_binder_nodesign_%A.err
+
+module switch PrgEnv-cray PrgEnv-intel
+module load daint-gpu
+export CRAYPE_LINK_TYPE=dynamic
+export OMP_NUM_THREADS=1
+
+ROSETTAPATH="/scratch/snx3000/jbonet/bin/main/source/bin/"
+ROSETTADIST="mpi.linuxiccrelease"
+TARGET="bcl2"
+PDB="pdbs/3lhp.pdb.gz"
+NSTRUCT=1000
+EXPERIMENT="ssecst"
+EXPCOMPLEMENT="binder_from4oyd_nodesign"
+JOBID=4
+SUFFIX=${EXPERIMENT}_${EXPCOMPLEMENT}_${JOBID}_${SLURM_TASK_PID}
+
+srun -n $SLURM_NTASKS --ntasks-per-node=$SLURM_NTASKS_PER_NODE -c $SLURM_CPUS_PER_TASK ${ROSETTAPATH}rosetta_scripts.${ROSETTADIST} -parser:protocol ${EXPERIMENT}.${EXPCOMPLEMENT}.xml @common_flags -s ${PDB} -out:nstruct ${NSTRUCT} -out:prefix ${TARGET}_ -out:suffix _${SUFFIX} -out:file:silent ${TARGET}_${SUFFIX} -mpi_tracer_to_file ./tracer_${SLURM_TASK_PID}.log
+
+echo "DAINT: RUN FINISHED"
+
