604d33536a1ff4a109b60413157e94238850db2e
Jaume Bonet <jaume.bonet@gmail.com>
2017-08-23 20:16:02 +0200

    prepared ilk classicFFL

--- benchmark/scientific/interleukin/experiments/from3qaz/classicFFL/submiter.castor.sh
+++ benchmark/scientific/interleukin/experiments/from3qaz/classicFFL/submiter.castor.sh
@@ -3,31 +3,30 @@
 #SBATCH --ntasks-per-node 1
 #SBATCH --cpus-per-task 1
 #SBATCH --mem 4000
-#SBATCH --job-name="il2"
+#SBATCH --job-name="il2classicFFL"
 #SBATCH --time 45:00:00
 #SBATCH --array=1-1000
-#SBATCH --output=/scratch/bonet/logs/il2.%A_%a.out
-#SBATCH --error=/scratch/bonet/logs/il2.%A_%a.err
+#SBATCH --output=/scratch/bonet/logs/il2classicFFL.%A_%a.out
+#SBATCH --error=/scratch/bonet/logs/il2classicFFL.%A_%a.err
 
 ROSETTAPATH="/scratch/lpdi/bin/Rosetta/devel/nubinitio/main/source/bin/"
 ROSETTADIST="linuxiccrelease"
-EXPCOMPLEMENT="nobinder"
+EXPCOMPLEMENT="classicFFL"
 JOBID=1
-NSTRUCT=40
 ROUND=${SLURM_ARRAY_TASK_ID}
-
-TARGET="il2"
+TARGET="design"
 DDG="evaluate_ddg"
-PDB="../../../complete_structure/3inkC_03.pdb"
 
+WITHBINDER="nobinder"
 if [ ${SLURM_ARRAY_TASK_ID} -gt 500 ]; then
-    EXPCOMPLEMENT="binder"
+    WITHBINDER="binder"
 fi
 
 SUFFIX=${EXPCOMPLEMENT}_${JOBID}_${ROUND}
-OUTDIR=$(echo ${EXPCOMPLEMENT})
+OUTDIR=${WITHBINDER}
+INSILENT=../nodesign/${WITHBINDER}/il2_${WITHBINDER}_1_${ROUND}
 mkdir -p ${OUTDIR}
-srun ${ROSETTAPATH}rosetta_scripts.${ROSETTADIST} -parser:protocol ${TARGET}.${EXPCOMPLEMENT}.xml @common_flags -s ${PDB} -out:nstruct ${NSTRUCT} -out:prefix ${TARGET}_ -out:suffix _${SUFFIX} -out:file:silent ${OUTDIR}/${TARGET}_${SUFFIX}
-srun ${ROSETTAPATH}rosetta_scripts.${ROSETTADIST} -parser:protocol ${DDG}.${EXPCOMPLEMENT}.xml @common_flags -in:file:silent ${OUTDIR}/${TARGET}_${SUFFIX} -in:file:silent_struct_type binary -out:file:silent ${OUTDIR}/${TARGET}_${SUFFIX}_${DDG}
 
+srun ${ROSETTAPATH}rosetta_scripts.${ROSETTADIST} -parser:protocol ${TARGET}.${EXPCOMPLEMENT}.xml @common_flags -in:file:silent ${INSILENT} -out:suffix _${SUFFIX} -out:file:silent ${OUTDIR}/${TARGET}_${SUFFIX}
+srun ${ROSETTAPATH}rosetta_scripts.${ROSETTADIST} -parser:protocol ${DDG}.${WITHBINDER}.xml @common_flags -in:file:silent ${OUTDIR}/${TARGET}_${SUFFIX} -in:file:silent_struct_type binary -out:file:silent ${OUTDIR}/${TARGET}_${SUFFIX}_${DDG}
 echo "CASTOR: RUN FINISHED"
