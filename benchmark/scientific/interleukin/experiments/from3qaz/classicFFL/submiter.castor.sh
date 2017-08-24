a21f35a61d75abf7f4da44f02d632e67810a0363
Jaume Bonet Martinez <jaume.bonet@gmail.com>
2017-08-24 16:00:08 +0200

    il2 nodesign-binder

--- benchmark/scientific/interleukin/experiments/from3qaz/classicFFL/submiter.castor.sh
+++ benchmark/scientific/interleukin/experiments/from3qaz/classicFFL/submiter.castor.sh
@@ -16,7 +16,7 @@ JOBID=1
 ROUND=${SLURM_ARRAY_TASK_ID}
 TARGET="design"
 DDG="evaluate_ddg"
-
+NSTRUCT=20
 WITHBINDER="nobinder"
 if [ ${SLURM_ARRAY_TASK_ID} -gt 500 ]; then
     WITHBINDER="binder"
@@ -24,9 +24,9 @@ fi
 
 SUFFIX=${EXPCOMPLEMENT}_${JOBID}_${ROUND}
 OUTDIR=${WITHBINDER}
-INSILENT=../nodesign/${WITHBINDER}/il2_${WITHBINDER}_1_${ROUND}
+    INSILENT=../nodesign/${WITHBINDER}/il2_${WITHBINDER}_1_${ROUND}
 mkdir -p ${OUTDIR}
 
-srun ${ROSETTAPATH}rosetta_scripts.${ROSETTADIST} -parser:protocol ${TARGET}.${EXPCOMPLEMENT}.xml @common_flags -in:file:silent ${INSILENT} -out:suffix _${SUFFIX} -out:file:silent ${OUTDIR}/${TARGET}_${SUFFIX}
+srun ${ROSETTAPATH}rosetta_scripts.${ROSETTADIST} -parser:protocol ${TARGET}.${EXPCOMPLEMENT}.xml @common_flags -in:file:silent ${INSILENT} -in:file:silent_select_random ${NSTRUCT} -out:suffix _${SUFFIX} -out:file:silent ${OUTDIR}/${TARGET}_${SUFFIX} -parser:script_vars tree=${WITHBINDER}.tree
 srun ${ROSETTAPATH}rosetta_scripts.${ROSETTADIST} -parser:protocol ${DDG}.${WITHBINDER}.xml @common_flags -in:file:silent ${OUTDIR}/${TARGET}_${SUFFIX} -in:file:silent_struct_type binary -out:file:silent ${OUTDIR}/${TARGET}_${SUFFIX}_${DDG}
 echo "CASTOR: RUN FINISHED"
